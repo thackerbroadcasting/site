ac93ad8461e9a12e5632dca27387e7342566e935
thackmaster <73045785+thackmaster@users.noreply.github.com>
2024-06-09 22:23:11 +0000

    maintenance message while research is conducted with the better uptime embed

--- src/navigation.js
+++ src/navigation.js
@@ -66,6 +66,6 @@ export const footerData = {
   ],
   footNote: `
     <!--<img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>-->
-    &copy 2024 Thacker Broadcasting · Site template by <a class="underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved
+    &copy 2024 Thacker Broadcasting • Site template by <a class="underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> • All rights reserved
   `,
 };
